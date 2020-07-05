21e3ae5434058f72b7f50251e7fa258692bb2cad
cyr-ius <cyr-ius@ipocus.net>
2020-07-05 16:20:01 +0200

    Fix header

--- plymouth-text.c
+++ plymouth-text.c
@@ -1,11 +1,3 @@
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
 #include "plymouth-lite.h"
 
 int main(int argc, char **argv)
